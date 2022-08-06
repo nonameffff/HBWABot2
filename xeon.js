f98d6fee999ee784f0d3f8fa4fad0d0ce2a14c7c
HBMods <105466507+HBMods1@users.noreply.github.com>
2022-08-06 13:47:15 +0530

    Add files via upload

--- xeon.js
+++ xeon.js
@@ -77,7 +77,7 @@ async function startXeonBotInc() {
     const XeonBotInc = XeonBotIncConnect({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
-        browser: ['Subscribe Xeon','Safari','1.0.0'],
+        browser: ['Subscribe HBMods','Safari','1.0.0'],
         auth: state
     })
 
